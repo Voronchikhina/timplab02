30da0983725a3b8536cf417894bd0f6c3f26e188
Voronchikhina <vdaria16@mail.ru>
2021-03-29 19:10:00 +0300

    added comments

--- sources/HelloWorld.cpp
+++ sources/HelloWorld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>;
 int main () {
 std::string ima;
-std::cin>>ima;
-std::cout << "Hello world"<<ima<<std::endl;
+std::cin>>ima;//Enter name
+std::cout << "Hello world"<<ima<<std::endl;//Output hello world and name
 return 0;
 }
