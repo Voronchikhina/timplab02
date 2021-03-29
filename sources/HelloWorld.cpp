2e4965f016a29f1da61e71636fa0046905085908
Voronchikhina <71675709+Voronchikhina@users.noreply.github.com>
2021-03-29 19:19:02 +0300

    Update HelloWorld.cpp

--- sources/HelloWorld.cpp
+++ sources/HelloWorld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>;
 int main () {
 std::string ima;
-std::cin>>ima;//Enter name
-std::cout << "Hello world"<<ima<<std::endl;//Output hello world and name
+std::cin>>ima;//Enter, name
+std::cout << "Hello world"<<ima<<std::endl;//Output hello world and имя
 return 0;
 }
